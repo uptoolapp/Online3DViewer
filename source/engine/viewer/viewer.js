5ac495f90488c79d566f413d3074d9738050f17b
Yang <54595761+yangcshen@users.noreply.github.com>
2025-12-26 13:06:26 -0800

    pinned to the latest version and use options

--- source/engine/viewer/viewer.js
+++ source/engine/viewer/viewer.js
@@ -634,200 +634,46 @@ export class Viewer
 
     GetGizmoOptions ()
     {
+        const faceConfig = {
+            color: 0xeeeeee,
+            labelColor: 0x000000,
+            hover: {
+                color: 0x2bc13e,
+            },
+        };
+        const edgeConfig = {
+            color: 0x999999,
+            opacity: 1,
+            hover: {
+                color: 0x2bc13e,
+            },
+        };
+        const cornerConfig = {
+            ...faceConfig,
+        };
+
         return {
-            type: 'sphere',
+            type: 'rounded-cube',
             size: 128,
             placement: 'bottom-right',
-            resolution: 64,
-            lineWidth: 4,
-            radius: 1,
-            smoothness: 18,
-            animated: true,
-            speed: 1,
-            background: {
-                enabled: true,
-                color: 16777215,
-                opacity: 0.1,
-                hover: {
-                color: 16777215,
-                opacity: 0.3
-                }
+            offset: {
+                top: 10,
+                left: 10,
+                bottom: 20,
+                right: 10,
             },
             font: {
                 family: 'sans-serif',
                 weight: 900
             },
-            offset: {
-                top: 10,
-                left: 10,
-                bottom: 20,
-                right: 10
-            },
-            corners: {
-                enabled: false,
-                color: 15915362,
-                opacity: 1,
-                scale: 0.15,
-                radius: 1,
-                smoothness: 18,
-                hover: {
-                color: 16777215,
-                opacity: 1,
-                scale: 0.2
-                }
-            },
-            edges: {
-                enabled: false,
-                color: 15915362,
-                opacity: 1,
-                radius: 1,
-                smoothness: 18,
-                scale: 0.15,
-                hover: {
-                color: 16777215,
-                opacity: 1,
-                scale: 0.2
-                }
-            },
-            x: {
-                enabled: true,
-                color: 16725587,
-                opacity: 1,
-                scale: 0.7,
-                labelColor: 2236962,
-                line: true,
-                border: {
-                size: 0,
-                color: 14540253
-                },
-                hover: {
-                color: 16777215,
-                labelColor: 2236962,
-                opacity: 1,
-                scale: 0.7,
-                border: {
-                    size: 0,
-                    color: 14540253
-                }
-                },
-                label: 'X'
-            },
-            y: {
-                enabled: true,
-                color: 9100032,
-                opacity: 1,
-                scale: 0.7,
-                labelColor: 2236962,
-                line: true,
-                border: {
-                size: 0,
-                color: 14540253
-                },
-                hover: {
-                color: 16777215,
-                labelColor: 2236962,
-                opacity: 1,
-                scale: 0.7,
-                border: {
-                    size: 0,
-                    color: 14540253
-                }
-                },
-                label: 'Y'
-            },
-            z: {
-                enabled: true,
-                color: 2920447,
-                opacity: 1,
-                scale: 0.7,
-                labelColor: 2236962,
-                line: true,
-                border: {
-                size: 0,
-                color: 14540253
-                },
-                hover: {
-                color: 16777215,
-                labelColor: 2236962,
-                opacity: 1,
-                scale: 0.7,
-                border: {
-                    size: 0,
-                    color: 14540253
-                }
-                },
-                label: 'Z'
-            },
-            nx: {
-                line: false,
-                scale: 0.45,
-                hover: {
-                scale: 0.5,
-                color: 16777215,
-                labelColor: 2236962,
-                opacity: 1,
-                border: {
-                    size: 0,
-                    color: 14540253
-                }
-                },
-                label: '',
-                enabled: true,
-                color: 16725587,
-                opacity: 1,
-                labelColor: 2236962,
-                border: {
-                size: 0,
-                color: 14540253
-                }
-            },
-            ny: {
-                line: false,
-                scale: 0.45,
-                hover: {
-                scale: 0.5,
-                color: 16777215,
-                labelColor: 2236962,
-                opacity: 1,
-                border: {
-                    size: 0,
-                    color: 14540253
-                }
-                },
-                label: '',
-                enabled: true,
-                color: 9100032,
-                opacity: 1,
-                labelColor: 2236962,
-                border: {
-                size: 0,
-                color: 14540253
-                }
-            },
-            nz: {
-                line: false,
-                scale: 0.45,
-                hover: {
-                scale: 0.5,
-                color: 16777215,
-                labelColor: 2236962,
-                opacity: 1,
-                border: {
-                    size: 0,
-                    color: 14540253
-                }
-                },
-                label: '',
-                enabled: true,
-                color: 2920447,
-                opacity: 1,
-                labelColor: 2236962,
-                border: {
-                size: 0,
-                color: 14540253
-                }
-            },
-            isSphere: true
+            corners: cornerConfig,
+            edges: edgeConfig,
+            right: faceConfig,
+            top: faceConfig,
+            front: faceConfig,
+            left: faceConfig,
+            bottom: faceConfig,
+            back: faceConfig,
         };
     }
 
